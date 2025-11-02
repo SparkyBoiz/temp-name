373a64314049ea392813e30f043e2e1429de5349
Dylan Morrison-Story <dylan.story@outlook.com>
2025-11-01 22:49:03 -0600

    Added Things
    - Added a Trap that plays an audio visual sound cue when a Ghost triggers it.

--- Alberta_GameJam/Assets/Scripts/Trap.cs
+++ Alberta_GameJam/Assets/Scripts/Trap.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     [SerializeField] private float trapDuration = 2f;
+    // Optional SoundWord prefab that represents the "creak" visual when the trap is triggered.
+    // Assign a prefab that has the `SoundWord` component (and a VisualEffect) in the Inspector.
+    // The prefab will be instantiated, `Spawn` will be called, and the instance will be
+    // destroyed after `soundWordDuration` seconds to avoid accumulating objects.
+    [SerializeField] private SoundWord soundWordPrefab;
+    [SerializeField] private float soundWordDuration = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Ghost ghost = other.GetComponent<Ghost>();
         if (ghost != null)
         {
+            // Spawn SoundWord visual (if provided)
+            if (soundWordPrefab != null)
+            {
+                // Instantiate the prefab and call Spawn to trigger the VisualEffect.
+                SoundWord sw = Instantiate(soundWordPrefab, transform.position, Quaternion.identity);
+                // direction and size are configurable; using upward direction and size 1 by default.
+                sw.Spawn(transform.position, Vector3.up, 1f);
+                // Destroy the spawned object after the configured duration so it doesn't linger.
+                Destroy(sw.gameObject, soundWordDuration);
+            }
+
             StartCoroutine(TrapGhost(ghost));
-            Collider trapCollider = GetComponent<Collider>();
+            // Use the 2D collider type here (this script is handling OnTriggerEnter2D)
+            Collider2D trapCollider = GetComponent<Collider2D>();
             if (trapCollider != null)
             {
                 trapCollider.enabled = false;
